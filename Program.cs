ab9f3630c85eaf8a391eedcc3c24aadf7baea7fe
CsUtil.com <45512166+cs-util@users.noreply.github.com>
2023-02-24 05:15:40 +0000

    First test to run the cscore lib in GitHub Codespaces, seems to work :)
    (cherry picked from commit 596fa89bbc769531fc19876455cbe220b157c3ae)

--- Program.cs
+++ Program.cs
@@ -1,34 +1,3 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using com.csutil;
 
-app.Urls.Add("http://localhost:5000");
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
-
-app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
-{
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
-}
-
-public record Weather
-{
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
-}
+Log.d("Hello World");
